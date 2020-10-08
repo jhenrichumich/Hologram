e57624da0a0950cfe11b050ede7ac6d7f662c71f
jhenrichumich <54868137+jhenrichumich@users.noreply.github.com>
2020-10-08 19:02:57 -0400

    Test

---
+++ avize/hologram/build/main.js
@@ -0,0 +1,52 @@
+window.__assets={"Meissen_Porcelain_Chandelier":"Meissen_Porcelain_Chandelier.jpg","background":"background.png","1200px-LetterX":"1200px-LetterX.svg.png"};(function(scope) {var __zxzgnpp7q = new Sky({color:"#d9f5ff",src:"https://l13.alamy.com/360/PN0J3G/hagia-sophia-museum-istanbul-turkey-PN0J3G.jpg"});var __l4j5ebw1x = new Image({position:"0.14 1.6 -0.4",rotation:"0 -30 0",scale:"0.2 0.25 0",src:"Meissen_Porcelain_Chandelier",visible:true});var __9rqy6s2tj = new CurvedImage({opacity:0.75,position:"0 1.6 -0.09",radius:"0.2",rotation:"0 30.19 0",scale:"1.88 0.37 2"});var __8tg813su1 = new Text({color:"#000000",position:"-0.14 1.7 -0.41",rotation:"1.76 -0.81 -1",scale:"0.2 0.2 0.1",text:"Avize"});var __2rr9jxsq2 = new Text({color:"#000000",position:"-0.2 1.66 -0.41",rotation:"1.76 -0.81 -1",scale:"0.1 0.1 0.1",text:"Translation: Chandelier"});var __jrbr66e2l = new Text({color:"#000000",position:"-0.2 1.63 -0.41",rotation:"1.76 -0.81 -1",scale:"0.1 0.1 0.1",text:"Pronunciation: A-vi-ze"});var __g3h3viejk = new Portal({href:"https://jhenrichumich.github.io/Hologram/index.html",image:null,position:"-0.08 1.55 -0.42",rotation:"0 0 0",scale:"0.05 0.05 0.01",title:"Return"});Object.assign(scope,{});})(window);/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map?hash=04d9e11dbd4632485b7a
