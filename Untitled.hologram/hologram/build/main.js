1970261fbfb493fa0cca4b606ad74e94c4115a3a
jhenrichumich <54868137+jhenrichumich@users.noreply.github.com>
2020-10-08 13:42:01 -0400

    Test

---
+++ Untitled.hologram/hologram/build/main.js
@@ -0,0 +1,52 @@
+window.__assets={"background":"background.png","Meissen_Porcelain_Chandelier":"Meissen_Porcelain_Chandelier.jpg"};(function(scope) {Hologram.scene.fog = null;var __sli680nhf = new Sky({rotation:"0 0 0",src:"background"});var __m2newtm3u = new Plane({opacity:1,position:"0 0 0",rotation:"-90 0 0",shadow:false});var __xa20g5j8t = new Plane({opacity:0,position:"0 0 0",rotation:"17 78 0",scale:"1 1 1",visible:true});var __ksatq1ns8 = new Portal({href:"a.html",image:null,position:"26 21 13",rotation:"22 45 19",scale:"1 1 1",title:"Avize",visible:true,parent:__xa20g5j8t});Object.assign(scope,{});})(window);/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map?hash=04d9e11dbd4632485b7a
