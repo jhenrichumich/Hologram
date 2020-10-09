7c3cdcf8b7483277860e1c00ae14efe67638b17f
jhenrichumich <54868137+jhenrichumich@users.noreply.github.com>
2020-10-08 21:21:18 -0400

    Test

---
+++ avize/avize-test.hologram/no.hologram/hologram/build/main.js
@@ -0,0 +1,52 @@
+window.__assets={"1200px-LetterX":"1200px-LetterX.svg.png","Meissen_Porcelain_Chandelier":"Meissen_Porcelain_Chandelier.jpg","background":"background.png","Untitled-MP3":"Untitled-MP3.mp3"};(function(scope) {var __zxzgnpp7q = new Sky({color:"#d9f5ff",src:"https://l13.alamy.com/360/PN0J3G/hagia-sophia-museum-istanbul-turkey-PN0J3G.jpg",visible:true});var __9rqy6s2tj = new CurvedImage({opacity:0.75,position:"0 1.6 -0.09",radius:"0.2",rotation:"0 30.19 0",scale:"1.88 0.37 2"});var __a3qegkytq = new Text({color:"#000000",position:"-0.07 1.7 -0.41",rotation:"1.76 -0.81 -1",scale:"0.15 0.15 0.1",text:"No"});var __gsxqt3uwd = new Portal({href:"https://jhenrichumich.github.io/Hologram/avize/avize-test.html",image:null,position:"-0.04 1.6 -0.42",rotation:"0 0 0",scale:"0.05 0.05 0.05",title:"Back"});Object.assign(scope,{});})(window);/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map?hash=04d9e11dbd4632485b7a
